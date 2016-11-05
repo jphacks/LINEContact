19e02b77f3704e3f7a67a37ad17c9c00585c6f5c
nappannda <qwe159753qwee@gmail.com>
2016-11-06 02:33:00 +0900

    line-login

--- line-contact/config/services.php
+++ line-contact/config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'line'   => [
+        'client_id'     => env('LINE_CLIENT_ID'),
+        'client_secret' => env('LINE_CLIENT_SECRET'),
+        'redirect'      => env('LINE_REDIRECT'),
+    ],
+
 ];
