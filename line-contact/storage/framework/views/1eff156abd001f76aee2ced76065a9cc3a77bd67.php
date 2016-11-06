0cebb8cd78ac6d64dcfc0837d1357ba1c86ba874
konojunya <junya03dance@gmail.com>
2016-11-06 19:16:06 +0900

    imageだけpush

---
+++ line-contact/storage/framework/views/1eff156abd001f76aee2ced76065a9cc3a77bd67.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+	<link rel="stylesheet" href="/css/app.css">
+
+	<script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+
+	<title>LINE CONTACT</title>
+</head>
+<body>
+	
+	<div id="react"></div>
+
+	<script src="/js/bundle.js"></script>
+</body>
+</html>
---
+++ line-contact/vendor/ringo-project/socialite/README.md
@@ -0,0 +1,8 @@
+# ringo-project/socialite
+
+* Laravel/Socialiteを拡張して、日本でよく使用されている以下のOAuth認証に対応しました。
+	* LINE
+	* Yahoo!JAPAN
+
+* また、既にLaravel/Socialiteに含まれている下記のproviderについて、genderを返すように変更しています。
+	* facebook
